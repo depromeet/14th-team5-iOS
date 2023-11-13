c566787065d8527d163a5b25a8781b189200b55e
Do-hyun-Kim <dohyun8032@gmail.com>
2023-11-13 22:49:10 +0900

    feat: 🤩 Tuist init Project Setting

---
+++ App/Project.swift
@@ -0,0 +1,29 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+import MyPlugin
+
+/*
+                +-------------+
+                |             |
+                |     App     | Contains App App target and App unit-test target
+                |             |
+         +------+-------------+-------+
+         |         depends on         |
+         |                            |
+ +----v-----+                   +-----v-----+
+ |          |                   |           |
+ |   Kit    |                   |     UI    |   Two independent frameworks to share code and start modularising your app
+ |          |                   |           |
+ +----------+                   +-----------+
+
+ */
+
+// MARK: - Project
+
+// Local plugin loaded
+let localHelper = LocalHelper(name: "MyPlugin")
+
+// Creates our project using a helper function defined in ProjectDescriptionHelpers
+let project = Project.app(name: "App",
+                          platform: .iOS,
+                          additionalTargets: ["AppKit", "AppUI"])

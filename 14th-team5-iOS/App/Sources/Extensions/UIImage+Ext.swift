d37d4d8c348d8459f31e797b9ca1e97f0b6e1289
Do-hyun-Kim <dohyun8032@gmail.com>
2023-12-07 23:12:12 +0900

    feat: UIImage+Extensions String Type Image Type으로 변환하는 Method 추가

---
+++ 14th-team5-iOS/App/Sources/Extensions/UIImage+Ext.swift
@@ -0,0 +1,25 @@
+//
+//  UIImage+Ext.swift
+//  App
+//
+//  Created by Kim dohyun on 12/7/23.
+//
+
+import UIKit
+
+
+extension UIImage {
+    public func combinedTextWithBackground(target text: String, size: CGSize, attributedString: [NSAttributedString.Key : Any]) -> UIImage {
+        let renderImage: UIGraphicsImageRenderer = UIGraphicsImageRenderer(size: size)
+        let targetText: NSString = (text as NSString)
+        let targetTextSize: CGSize = targetText.size(withAttributes: attributedString)
+        let targetRect: CGRect = CGRect(x: (size.width - targetTextSize.width) / 2, y: (size.height - targetTextSize.height) - 10, width: 100, height: 100)
+        let originalImage: UIImage = renderImage.image { _ in
+            self.draw(in: CGRect(origin: .zero, size: size))
+            targetText.size(withAttributes: attributedString)
+            targetText.draw(in: targetRect, withAttributes: attributedString)
+        }
+        return originalImage
+    }
+    
+}

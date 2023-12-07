ba03e9ffcc1565d34c6c7144cd25154d44d47274
Zizi_Kim <rlarjsdn3@naver.com>
2023-12-07 23:48:25 +0900

    feat: 메인 캘린더 UI 구현 (#30)

--- 14th-team5-iOS/App/Sources/Presentation/Calendar/View/ImageCalendarCell.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ImageCalendarCell.swift
-//  App
-//
-//  Created by 김건우 on 12/6/23.
-//
-
-import Foundation
--- 14th-team5-iOS/App/Sources/Presentation/Calendar/View/PlaceholderCalendarCell.swift
+++ 14th-team5-iOS/App/Sources/Presentation/Calendar/View/PlaceholderCalendarCell.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+import Core
+import FSCalendar
+import Kingfisher
+import ReactorKit
+import RxCocoa
+import RxSwift
+import SnapKit
+import Then
+
+final class PlaceholderCalendarCell: FSCalendarCell {
+    // MARK: - Properties
+    static let identifier: String = "PlaceholderCalendarCell"
+    
+    // MARK: - Intializer
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        setupAutoLayout()
+    }
+    
+    required init(coder aDecoder: NSCoder!) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Helpers
+    func setupAutoLayout() {
+        titleLabel.snp.makeConstraints {
+            $0.center.equalTo(contentView.snp.center)
+        }
+    }
+}

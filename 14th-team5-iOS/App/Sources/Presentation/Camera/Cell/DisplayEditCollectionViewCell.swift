55a37572190891e153498413312d3e7a8efc7fda
Do-hyun-Kim <dohyun8032@gmail.com>
2023-12-14 18:23:19 +0900

    feat: DisplayEditCollectionViewCell UI Binding Code 추가 - EditCollectionViewCell UI Configure

--- 14th-team5-iOS/App/Sources/Presentation/Camera/Cell/DisplayEditCollectionViewCell.swift
+++ 14th-team5-iOS/App/Sources/Presentation/Camera/Cell/DisplayEditCollectionViewCell.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 import Core
+import RxSwift
+import RxCocoa
+import ReactorKit
+import SnapKit
+import Then
 
 
 public final class DisplayEditCollectionViewCell: BaseCollectionViewCell<DisplayEditCellReactor> {
@@ -18,14 +23,48 @@ public final class DisplayEditCollectionViewCell: BaseCollectionViewCell<Display
     
     public override func setupUI() {
         super.setupUI()
-        self.contentView.backgroundColor = .black
+        self.contentView.addSubviews(containerView, descrptionLabel)
     }
     
     public override func setupAttributes() {
+        super.setupAttributes()
+        
+        containerView.do {
+            $0.backgroundColor = .black.withAlphaComponent(0.71)
+            $0.layer.cornerRadius = 8
+            $0.clipsToBounds = true
+        }
+        
+        descrptionLabel.do {
+            $0.textColor = .white
+            $0.textAlignment = .center
+            $0.font = .systemFont(ofSize: 32, weight: .regular)
+        }
+        
         
     }
     
     public override func setupAutoLayout() {
+        super.setupAutoLayout()
+        
+        containerView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        descrptionLabel.snp.makeConstraints {
+            $0.width.equalTo(30)
+            $0.height.equalTo(21)
+            $0.center.equalTo(containerView)
+        }
+    }
+    
+    
+    public override func bind(reactor: DisplayEditCellReactor) {
+        
+        reactor.state
+            .map { $0.title }
+            .bind(to: descrptionLabel.rx.text)
+            .disposed(by: disposeBag)
         
     }
     

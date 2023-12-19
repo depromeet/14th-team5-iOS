3afaf1e6d277fd23868bce8b2ecee2e6b23a5cc9
Do-hyun-Kim <dohyun8032@gmail.com>
2023-12-19 18:57:20 +0900

    fix: DesignSystem Assets WireFrame Image 제거 및 원본 이미지 추가 - Reactive+Ext PanGesture ControlEvent 추가 - CameraViewController TransitionImageScaleMethod 추가

--- 14th-team5-iOS/App/Sources/Application/SceneDelegate.swift
+++ 14th-team5-iOS/App/Sources/Application/SceneDelegate.swift
@@ -20,7 +20,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     ) {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        window?.rootViewController = UINavigationController(rootViewController: HomeViewController(reacter: HomeViewReactor()))
+        let cameraDIContainer = CameraDIContainer().makeViewController()
+        window?.rootViewController = UINavigationController(rootViewController: cameraDIContainer)
         window?.makeKeyAndVisible()
     }
 }

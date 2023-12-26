f9933c83a522720d6f4f248d038972ba91b05669
Do-hyun-Kim <dohyun8032@gmail.com>
2023-12-26 16:45:16 +0900

    feat: ProfilePostDTO, Response, Parameter 추가

---
+++ 14th-team5-iOS/Domain/Sources/Profile/DataMapping/ProfilePostDTO.swift
@@ -0,0 +1,59 @@
+//
+//  ProfilePostDTO.swift
+//  Domain
+//
+//  Created by Kim dohyun on 12/26/23.
+//
+
+import Foundation
+
+import Core
+
+
+public struct ProfilePostDTO: Decodable {
+    public var currentPage: Int
+    public var totalPage: Int
+    public var itemPerPage: Int
+    public var hasNext: Bool
+    public var results: [ProfilePostResponseDTO]
+}
+
+extension ProfilePostDTO {
+    public struct ProfilePostResponseDTO: Decodable {
+        public var postId: String
+        public var authorId: String
+        public var commentCount: Int
+        public var emojiCount: Int
+        public var imageUrl: String
+        public var content: String
+        public var createdAt: String
+    }
+    
+}
+
+
+extension ProfilePostDTO {
+    public func toDomain() -> ProfilePostResponse {
+        return .init(
+            currentPage: currentPage,
+            totalPage: totalPage,
+            itemPerPage: itemPerPage,
+            hasNext: hasNext,
+            results: results.map { $0.toDomain() }
+        )
+    }
+}
+
+extension ProfilePostDTO.ProfilePostResponseDTO {
+    public func toDomain() -> ProfilePostResultResponse {
+        return .init(
+            postId: postId,
+            authorId: authorId,
+            commentCount: "\(commentCount)",
+            emojiCount: "\(emojiCount)",
+            imageUrl: URL(string: imageUrl) ?? URL(fileURLWithPath: ""),
+            content: content,
+            createdAt: createdAt.toDate(with: "yyyy년 M월 D일")
+        )
+    }
+}

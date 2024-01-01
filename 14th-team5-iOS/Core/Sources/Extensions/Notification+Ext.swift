25e8b7c17e7e53541c46b684eaa71871251fb710
Do-hyun-Kim <dohyun8032@gmail.com>
2024-01-02 02:29:25 +0900

    feat: 유저 삭제애 대한 Notification Name 추가

--- 14th-team5-iOS/Core/Sources/Extensions/Notification+Ext.swift
+++ 14th-team5-iOS/Core/Sources/Extensions/Notification+Ext.swift
@@ -12,5 +12,5 @@ extension Notification.Name {
     public static let AVCapturePhotoOutputDidFinishProcessingPhotoNotification: Notification.Name = Notification.Name("AVCapturePhotoOutputDidFinishProcessingPhotoNotification")
     public static let PHPickerAssetsDidFinishPickingProcessingPhotoNotification: Notification.Name = Notification.Name("PHPickerAssetsDidFinishPickingProcessingPhotoNotification")
     public static let AppVersionsCheckWithRedirectStore = Notification.Name("AppVersionsCheckWithRedirectStore")
-    
+    public static let UserAccountDeleted = Notification.Name("UserAccountDeleted")
 }

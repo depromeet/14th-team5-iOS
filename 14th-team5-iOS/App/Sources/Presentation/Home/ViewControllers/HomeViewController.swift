3cdd1d1f2186f6973a080ea5f2a06f6f29d6063b
Kyoungmi <1mmgm.dev@gmail.com>
2024-01-16 15:12:30 +0900

    fix: fix the timer(#284)

--- 14th-team5-iOS/App/Sources/Presentation/Home/ViewControllers/HomeViewController.swift
+++ 14th-team5-iOS/App/Sources/Presentation/Home/ViewControllers/HomeViewController.swift
@@ -283,16 +283,6 @@ extension HomeViewController {
             .withUnretained(self)
             .bind(onNext: { $0.0.setNoPostTodayView($0.1) })
             .disposed(by: disposeBag)
-        
-        reactor.state
-            .map { $0.didPost }
-            .observe(on: MainScheduler.instance)
-            .distinctUntilChanged()
-            .withUnretained(self)
-            .bind(onNext: {
-                $0.0.hideCameraButton($0.1)
-            })
-            .disposed(by: disposeBag)
     }
 }
 

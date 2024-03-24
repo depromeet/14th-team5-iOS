4a0571c48de48aa852d30f3868d257d3903d9761
Do-hyun-Kim <dohyun8032@gmail.com>
2024-03-24 19:11:06 +0900

    fix: CFBundleShortVersionString 1.1.8 수정 - FastFile .env 환경변수 기존 네이밍으로 수정 - MathFile 프로비저닝 파일로 변경

--- 14th-team5-iOS/App/Project.swift
+++ 14th-team5-iOS/App/Project.swift
@@ -19,7 +19,7 @@ private let targets: [Target] = [
                 "CFBundleDisplayName": .string("Bibbi"),
                 "CFBundleVersion": .string("1"),
                 "CFBuildVersion": .string("0"),
-                "CFBundleShortVersionString": .string("1.1.7"),
+                "CFBundleShortVersionString": .string("1.1.8"),
                 "UILaunchStoryboardName": .string("LaunchScreen.storyboard"),
                 "UISupportedInterfaceOrientations": .array([.string("UIInterfaceOrientationPortrait")]),
                 "UIUserInterfaceStyle": .string("Light"),

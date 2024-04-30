ec07f1f6e0d46014ff1123c6a2e31067186be92d
Kyoungmi <1mmgm.dev@gmail.com>
2024-04-30 19:29:54 +0900

    [feat]: fix main view api (#490)

--- 14th-team5-iOS/Domain/Sources/Main/Entities/MainData.swift
+++ 14th-team5-iOS/Domain/Sources/Main/Entities/MainData.swift
@@ -20,15 +20,27 @@ public struct Picker {
 }
 
 public struct MainData {
-    public let mainFamilyProfileDatas: [ProfileData]
-    let isMissionUnlocked: Bool
-    let isMeUploadedToday: Bool
-    let pickers: [Picker]
+    public let mainFamilyProfileDatas: [FamilySection.Item]
+    public let leftUploadCountUntilMissionUnlock: Int
+    public let isMissionUnlocked: Bool
+    public let isMeSurvivalUploadedToday: Bool
+    public let isMeMissionUploadedToday: Bool
+    public let pickers: [Picker]
     
-    public init(mainFamilyProfileDatas: [ProfileData], isMissionUnlocked: Bool, isMeUploadedToday: Bool, pickers: [Picker]) {
-        self.mainFamilyProfileDatas = mainFamilyProfileDatas
+    public init(mainFamilyProfileDatas: [ProfileData], leftUploadCountUntilMissionUnlock: Int, isMissionUnlocked: Bool, isMeSurvivalUploadedToday: Bool, isMeMissionUploadedToday: Bool, pickers: [Picker]) {
+        self.mainFamilyProfileDatas = mainFamilyProfileDatas.map(FamilySection.Item.main)
+        self.leftUploadCountUntilMissionUnlock = leftUploadCountUntilMissionUnlock
         self.isMissionUnlocked = isMissionUnlocked
-        self.isMeUploadedToday = isMeUploadedToday
+        self.isMeSurvivalUploadedToday = isMeSurvivalUploadedToday
+        self.isMeMissionUploadedToday = isMeMissionUploadedToday
         self.pickers = pickers
     }
+    
+    
+//    public init(mainFamilyProfileDatas: [ProfileData], isMissionUnlocked: Bool, isMeUploadedToday: Bool, pickers: [Picker]) {
+//        self.mainFamilyProfileDatas = mainFamilyProfileDatas.map(FamilySection.Item.main)
+//        self.isMissionUnlocked = isMissionUnlocked
+//        self.isMeUploadedToday = isMeUploadedToday
+//        self.pickers = pickers
+//    }
 }

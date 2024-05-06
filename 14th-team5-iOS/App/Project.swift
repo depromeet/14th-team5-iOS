e1ac81ed41e00170b60df2445fa92d497e6b33ec
Do-hyun-Kim <dohyun8032@gmail.com>
2024-05-06 15:59:26 +0900

    feat: swift.yml startsWIth 메서드로 수정 - Project.swift CFBundleShortVersionString Major로 변경

--- 14th-team5-iOS/App/Project.swift
+++ 14th-team5-iOS/App/Project.swift
@@ -19,7 +19,7 @@ private let targets: [Target] = [
                 "CFBundleDisplayName": .string("Bibbi"),
                 "CFBundleVersion": .string("1"),
                 "CFBuildVersion": .string("0"),
-                "CFBundleShortVersionString": .string("1.2"),
+                "CFBundleShortVersionString": .string("2.0"),
                 "UILaunchStoryboardName": .string("LaunchScreen.storyboard"),
                 "UISupportedInterfaceOrientations": .array([.string("UIInterfaceOrientationPortrait")]),
                 "UIUserInterfaceStyle": .string("Light"),

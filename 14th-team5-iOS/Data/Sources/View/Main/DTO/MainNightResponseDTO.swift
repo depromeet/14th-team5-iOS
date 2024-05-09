06f5aa864323473f92d9ddccc51f7aa53c9c0357
Kyoungmi <1mmgm.dev@gmail.com>
2024-05-09 22:38:23 +0900

    [feat]: add Images

---
+++ 14th-team5-iOS/Data/Sources/View/Main/DTO/MainNightResponseDTO.swift
@@ -0,0 +1,47 @@
+//
+//  MainNightResponseDTO.swift
+//  Data
+//
+//  Created by 마경미 on 07.05.24.
+//
+
+import Foundation
+
+import Domain
+
+struct Ranker: Codable {
+    let profileImageUrl: String
+    let name: String
+    let survivalCount: Int
+    
+    func toDomain() -> RankerData {
+        return .init(imageURL: profileImageUrl, name: name, survivalCount: survivalCount)
+    }
+}
+
+struct FamilyMemberMonthlyRanking: Codable {
+    let month: Int
+    let firstRanker: Ranker
+    let secondRanker: Ranker
+    let thirdRanker: Ranker
+    let mostRecentSurvivalPostDate: String
+    
+    func toDomain() -> FamilyRankData {
+        return .init(month: month,
+                     recentPostDate: mostRecentSurvivalPostDate,
+                     firstRanker: firstRanker.toDomain(),
+                     secondRanker: secondRanker.toDomain(),
+                     thirdRanker: thirdRanker.toDomain()
+        )
+    }
+}
+
+struct MainNightResponseDTO: Codable {
+    let topBarElements: [TopBarElement]
+    let familyMemberMonthlyRanking: FamilyMemberMonthlyRanking
+    
+    func toDomain() -> MainNightData {
+        return .init(mainFamilyProfileDatas: topBarElements.map { $0.toDomain() },
+                     familyRankData: familyMemberMonthlyRanking.toDomain())
+    }
+}

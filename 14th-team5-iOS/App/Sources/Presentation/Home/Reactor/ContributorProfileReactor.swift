21e660ce2ad3a8aa8b86f70d8921b406271399c3
Kyoungmi <1mmgm.dev@gmail.com>
2024-05-09 22:38:23 +0900

    [feat]: add Images

---
+++ 14th-team5-iOS/App/Sources/Presentation/Home/Reactor/ContributorProfileReactor.swift
@@ -0,0 +1,26 @@
+//
+//  ContributorProfileReactor.swift
+//  App
+//
+//  Created by 마경미 on 07.05.24.
+//
+
+import Foundation
+
+import ReactorKit
+
+class ContributorProfileReactor: Reactor {
+    enum Action {
+        
+    }
+    
+    enum Mutation {
+        
+    }
+
+    struct State {
+        
+    }
+    
+    let initialState: State = State()
+}

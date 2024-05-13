f000b124c26ff4ac253f36394cc16566dccd89fe
Kyoungmi <1mmgm.dev@gmail.com>
2024-05-13 19:32:51 +0900

    [feat]: segment swipe(#526)

--- 14th-team5-iOS/App/Sources/Presentation/Home/Reactor/MainViewReactor.swift
+++ 14th-team5-iOS/App/Sources/Presentation/Home/Reactor/MainViewReactor.swift
@@ -240,7 +240,8 @@ extension MainViewReactor {
             if currentState.pageIndex == 0 {
                 newState.cameraEnabled = !currentState.isMeSurvivalUploadedToday
             } else {
-                if !currentState.isMeMissionUploadedToday && currentState.isMissionUnlocked {
+
+                    if currentState.isMeSurvivalUploadedToday && (!currentState.isMeMissionUploadedToday) && currentState.isMissionUnlocked {
                     newState.cameraEnabled = true
                 } else {
                     newState.cameraEnabled = false

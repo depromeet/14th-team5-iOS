7586d0b956c3d38e15c15667e1e97283d8b1a05c
Zizi_Kim <rlarjsdn3@naver.com>
2024-06-05 21:32:17 +0900

    feat: DailyCalendarViewController에 예시 코드 작성 (#546)

--- 14th-team5-iOS/Core/Sources/Base/BBNavigationViewController.swift
+++ 14th-team5-iOS/Core/Sources/Base/BBNavigationViewController.swift
@@ -14,10 +14,13 @@ import ReactorKit
 ///
 /// 이 ViewController는 ReactorViewController를 상속하고 있으며, bind() 메서드또한 제공합니다.
 ///
-/// 삐삐 스타일의 NavigationBar의 UI나 스타일을 변경해야 한다면, 직접 해당 NavigationBar에 정의되어 있는 메서드나 프로퍼티를 통해 변경할 수 있습니다. 가령, NavigationBar의 높이를 바꿔야 한다면. `setHeight(_:)` 메서드를 호출하면 됩니다.
+/// 이 ViewController를 상속하는 ViewController는 `View`가 아닌 `ContentView`에 UI를 배치해야 합니다. 
+/// `ContentView`는 NavigationBar 영역을 제외한 나머지 공간을 차지하는 View입니다. 이 View는 NavigationBar의 높이에 따라 동적으로 변합니다.
 ///
-/// LeftBarButton의 타입이 .arrowLeft나 .xmark라면 popViewController() 구현이 기본으로 제공됩니다.
+/// 삐삐 스타일의 NavigationBar의 UI나 스타일을 변경해야 한다면, 직접 해당 NavigationBar에 정의되어 있는 메서드나 프로퍼티를 통해 변경할 수 있습니다. 
+/// 가령, NavigationBar의 높이를 바꿔야 한다면. `setHeight(_:)` 메서드를 호출하면 됩니다.
 ///
+/// LeftBarButton의 타입이 .arrowLeft나 .xmark라면 popViewController() 구현이 기본으로 제공됩니다. 
 /// 왼쪽, 오른쪽 버튼의 동작을 정의하고 싶다면 `navigationBarView.rx.didTapRightButton`과 같이 정의하면 됩니다.
 ///
 /// - NOTE: BBNavigationBarView의 스타일 변경을 위해 미리 정의된 편리한 메서드는 해당 뷰의 퀵헬프를 참조해주세요.
@@ -81,6 +84,7 @@ open class BBNavigationViewController<R>: ReactorViewController<R> where R: Reac
             $0.horizontalEdges.equalToSuperview()
             $0.bottom.equalToSuperview()
         }
+
     }
     
     open override func setupAttributes() {
@@ -94,6 +98,7 @@ open class BBNavigationViewController<R>: ReactorViewController<R> where R: Reac
 
 extension BBNavigationViewController {
     
+    /// NavigationBar의 높이를 바꿉니다.
     public func setHeight(_ height: CGFloat) {
         navigationBarView.snp.updateConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide)
@@ -106,6 +111,12 @@ extension BBNavigationViewController {
             $0.horizontalEdges.equalToSuperview()
             $0.bottom.equalToSuperview()
         }
+    }
+    
+    /// NavigationBar를 View의 맨 앞으로 가져옵니다.
+    /// contentView가 아닌 view에 새로운 UI를 배치할 때, 꼭 호출해주어야 합니다.
+    public func bringNavigationBarViewToFront() {
+        view.bringSubviewToFront(navigationBarView)
     }
     
 }

56b2b9a365f33ecbf705ed9cf681c2695eb21850
Zizi_Kim <rlarjsdn3@naver.com>
2024-07-10 19:42:23 +0900

    feat: App 관련 통신 및 의존성 주입 구현 (#589)

--- 14th-team5-iOS/App/Sources/Application/DIContainer/AppDIContainer.swift
+++ 14th-team5-iOS/App/Sources/Application/DIContainer/AppDIContainer.swift
@@ -7,12 +7,33 @@
 
 import Core
 import Data
-import Foundation
+import Domain
 
 final class AppDIContainer: BaseContainer {
     
+    // MARK: - Make UseCase
+    
+    private func makeFetchAppVersionUseCase() -> FetchAppVersionUseCaseProtocol {
+        FetchAppVersionUseCase(
+            appRepository: makeAppRepository()
+        )
+    }
+    
+    
+    // MARK: - Make Repository
+    
+    private func makeAppRepository() -> AppRepositoryProtocol {
+        return AppRepository()
+    }
+    
+    
     // MARK: - Register
+    
     func registerDependencies() {
+        
+        container.register(type: FetchAppVersionUseCaseProtocol.self) { _ in
+            self.makeFetchAppVersionUseCase()
+        }
         
         // ServiceProvider 등록
         container.register(type: GlobalStateProviderProtocol.self) { _ in

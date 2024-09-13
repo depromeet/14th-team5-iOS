35bd9e2b80799009a53c06b2e484f8181f74ebb4
Do-hyun-Kim <dohyun8032@gmail.com>
2024-09-14 01:35:23 +0900

    feat: BBToolTipView 추가

---
+++ 14th-team5-iOS/Core/Sources/Bibbi/BBCommons/BBToolTip/BBToolTipView.swift
@@ -0,0 +1,18 @@
+//
+//  BBToolTipView.swift
+//  Core
+//
+//  Created by Kim dohyun on 9/13/24.
+//
+
+import Foundation
+
+
+import UIKit
+
+
+final class BBToolTipView: UIView {
+    
+    
+    
+}

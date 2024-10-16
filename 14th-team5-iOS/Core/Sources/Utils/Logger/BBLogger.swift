4e9be4f1f6e96ad26da5f21d46eaa1e00e52647b
Do-hyun-Kim <dohyun8032@gmail.com>
2024-10-17 00:15:38 +0900

    comment: BBLogger 주석 추가

--- 14th-team5-iOS/Core/Sources/Utils/Logger/BBLogger.swift
+++ 14th-team5-iOS/Core/Sources/Utils/Logger/BBLogger.swift
@@ -10,11 +10,17 @@ import os
 
 import RxSwift
 
-
+/// LogLevel은 **OSLogType** 기준으로 정의를 했습니다.
+/// 해당 **LogLevel** 을 통해서 BBLogger의  출력 메세지를 지정 할 수 있습니다.
 private enum LogLevel {
+    /// 일반적인 정보를 조회할 때 사용하는 Type
+    /// ex) 네트워크 요청 시 Response 값 조회
     case info
+    /// 코드 디버깅할 때 사용하는 Type
     case debug
+    /// 경고 오류가 발생했을 때 사용하는 Type
     case error
+    /// 크래시를 유발하는 오류 나타낼 때 사용하는 Type
     case fault
     
     
@@ -30,6 +36,12 @@ private enum LogLevel {
 
 
 public struct BBLogger {
+    /// 일반적인 정보를 조회할때 사용하는 메서드 입니다.
+    /// - Parameters:
+    ///   - function: logInfo 메서드를 호출한 함수명
+    ///   - fileName: logInfo 메서드를 호출한 파일명
+    ///   - category: 로그의 카테고리 예) "Network", "UI", "UseCase",
+    ///   - message:  로그 메세지
     public static func logInfo(
         function: String = #function,
         fileName: String = #file,
@@ -51,7 +63,12 @@ public struct BBLogger {
             .info("\(infotMessage)")
     }
     
-    
+    /// 코드 디버깅할때 사용하는 메서드 입니다..
+    /// - Parameters:
+    ///   - function: logInfo 메서드를 호출한 함수명
+    ///   - fileName: logInfo 메서드를 호출한 파일명
+    ///   - category: 로그의 카테고리 예) "Network", "UI", "UseCase",
+    ///   - message:  로그 메세지
     public static func logDebug(
         function: String = #function,
         fileName: String = #file,
@@ -74,8 +91,13 @@ public struct BBLogger {
             .debug("\(debugMessage)")
     
     }
-    
-    
+   
+    /// 경고 오류를 기록 및 추적할 때 사용하는 메서드입니다.
+    /// - Parameters:
+    ///   - function: logInfo 메서드를 호출한 함수명
+    ///   - fileName: logInfo 메서드를 호출한 파일명
+    ///   - category: 로그의 카테고리 예) "Network", "UI", "UseCase",
+    ///   - message:  로그 메세지
     public static func logError(
         function: String = #function,
         fileName: String = #file,
@@ -86,7 +108,7 @@ public struct BBLogger {
             fatalError("Bundle ID value not found")
         }
         
-        let errortMessage = createLoggerMessage(
+        let errorMessage = createLoggerMessage(
             level: LogLevel.error,
             message: message,
             function: function,
@@ -94,9 +116,15 @@ public struct BBLogger {
         )
         
         Logger(subsystem: bundleId, category: category)
-            .error("\(errortMessage)")
+            .error("\(errorMessage)")
     }
     
+    /// 크래시 오류를 추적 및 기록할 때 사용하는 메서드입니다.
+    /// - Parameters:
+    ///   - function: logInfo 메서드를 호출한 함수명
+    ///   - fileName: logInfo 메서드를 호출한 파일명
+    ///   - category: 로그의 카테고리 예) "Network", "UI", "UseCase",
+    ///   - message:  로그 메세지
     public static func logFault(
         function: String = #function,
         fileName: String = #file,
@@ -122,6 +150,12 @@ public struct BBLogger {
 
 extension BBLogger {
     
+    /// 로그 메서드 내부 출력 구문을 생성하기 위한 메서드입니다.
+    /// - Parameters:
+    ///   - level: LogLevel Type
+    ///   - message: 로그 메세지
+    ///   - function: logInfo 메서드를 호출한 함수명
+    ///   - fileName: logInfo 메서드를 호출한 파일명
     private static func createLoggerMessage(
         level: LogLevel,
         message: String,
@@ -141,6 +175,10 @@ extension BBLogger {
 
 
 extension ObservableType {
+    /// RxSwift에서 제공되는. debug 연산자와 동일하게 element를 로깅 하는 연산자입니다.
+    /// 해당 연산자를 통해서 BBLogger를 사용하여 로깅을 할 수 있습니다.
+    /// - Parameters:
+    ///    - category: 로그의 카테고리 예) "Network", "UI", "UseCase",
     public func log(_ category: String) -> Observable<Element> {
         return self.do(onNext: { element in
             BBLogger.logDebug(category: category, message: "\(element)")

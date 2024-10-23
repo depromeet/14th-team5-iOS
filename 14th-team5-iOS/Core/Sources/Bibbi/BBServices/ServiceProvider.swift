851ff0da3f2b7b0d05df6c033f3c34027cebe2ea
Zizi_Kim <rlarjsdn3@naver.com>
2024-10-23 09:49:55 +0900

    delete: 필요없는 Service 삭제

--- 14th-team5-iOS/Core/Sources/Bibbi/BBServices/ServiceProvider.swift
+++ 14th-team5-iOS/Core/Sources/Bibbi/BBServices/ServiceProvider.swift
@@ -17,8 +17,6 @@ public protocol ServiceProviderProtocol: AnyObject {
     var managementService: ManagementServiceType { get }
     
     var postGlobalState: PostGlobalStateType { get }
-    var toastGlobalState: ToastMessageGlobalStateType { get }
-    var profileGlobalState: ProfileGlobalStateType { get }
     var timerGlobalState: TimerGlobalStateType { get }
     var realEmojiGlobalState: RealEmojiGlobalStateType { get }
     var profilePageGlobalState: ProfileFeedGlobalStateType { get }
@@ -34,8 +32,6 @@ final public class ServiceProvider: ServiceProviderProtocol {
     public lazy var managementService: any ManagementServiceType = ManagementService(provider: self)
     
     public lazy var postGlobalState: PostGlobalStateType = PostGlobalState(provider: self)
-    public lazy var toastGlobalState: ToastMessageGlobalStateType = ToastMessageGlobalState(provider: self)
-    public lazy var profileGlobalState: ProfileGlobalStateType = ProfileGlobalState(provider: self)
     public lazy var timerGlobalState: TimerGlobalStateType = TimerGlobalState(provider: self)
     public lazy var realEmojiGlobalState: RealEmojiGlobalStateType = RealEmojiGlobalState(provider: self)
     public lazy var profilePageGlobalState: ProfileFeedGlobalStateType = ProfileFeedGlobalState(provider: self)

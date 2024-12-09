6912296b2e7b45c4fcc39627528ad5a704c50882
Do-hyun-Kim <dohyun8032@gmail.com>
2024-12-10 00:48:11 +0900

    fix: UpdateFamilyNameRequestDTO encode Method 추가

--- 14th-team5-iOS/Data/Sources/APIs/Family/DataMapping/UpdateFamilyNameRequestDTO.swift
+++ 14th-team5-iOS/Data/Sources/APIs/Family/DataMapping/UpdateFamilyNameRequestDTO.swift
@@ -9,4 +9,13 @@ import Foundation
 
 public struct UpdateFamilyNameRequestDTO: Encodable {
     let familyName: String?
+    
+    private enum CodingKeys: String, CodingKey {
+        case familyName
+    }
+    
+    public func encode(to encoder: any Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(familyName, forKey: .familyName)
+    }
 }

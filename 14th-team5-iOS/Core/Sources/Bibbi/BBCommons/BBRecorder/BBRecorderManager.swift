55cb000417bd8fad9dfb0e8c1283270d3a22ccaa
Do-hyun-Kim <dohyun8032@gmail.com>
2024-12-23 20:38:54 +0900

    feat: BBRecorderCore, BBRecorderManger, BBRecorderOption AVAudioRecorder, AVAudioPlayer를 사용하기 위한 클래스들 추상화

---
+++ 14th-team5-iOS/Core/Sources/Bibbi/BBCommons/BBRecorder/BBRecorderManager.swift
@@ -0,0 +1,66 @@
+//
+//  BBRecorderManager.swift
+//  Core
+//
+//  Created by 김도현 on 12/23/24.
+//
+
+import Foundation
+
+import AVFoundation
+
+public class BBRecorderManager: NSObject {
+    private var recorderCore: BBRecorderCore
+
+    init(
+        recorderCore: BBRecorderCore = BBRecorderCore()
+    ) {
+        self.recorderCore = recorderCore
+        super.init()
+        start()
+    }
+
+    
+    @discardableResult
+    public func start() -> Self {
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playAndRecord, options: .defaultToSpeaker)
+            try AVAudioSession.sharedInstance().setActive(true)
+            try AVAudioSession.sharedInstance().overrideOutputAudioPort(.speaker)
+        } catch {
+            print("AVAudio Session Not Settings")
+        }
+        return self
+    }
+    
+    @objc @discardableResult
+    public func startRecoding() -> Self {
+        print("start recording")
+        recorderCore.audioRecorder.record()
+        print("session audioRecoder : \(recorderCore.audioRecorder.settings)")
+        return self
+    }
+    
+    @discardableResult
+    public func pauseRecoding() -> Self {
+        recorderCore.audioRecorder.pause()
+        return self
+    }
+    
+    
+    @objc @discardableResult
+    public func stopRecoding() -> Self {
+        print("stop recording")
+        recorderCore.audioRecorder.stop()
+        return self
+    }
+    
+    @discardableResult
+    public func play() -> Self {
+        recorderCore.audioPlayer.volume = 1.0
+        recorderCore.audioPlayer.prepareToPlay()
+        recorderCore.audioPlayer.play()
+        return self
+    }
+    
+}
---
+++ 14th-team5-iOS/Core/Sources/Bibbi/BBCommons/BBRecorder/BBRecorderOption.swift
@@ -0,0 +1,88 @@
+//
+//  BBRecorderOption.swift
+//  Core
+//
+//  Created by 김도현 on 12/23/24.
+//
+
+import Foundation
+
+import AVFoundation
+
+public enum BBRecorderOption {
+    case formatIDKey(AudioFormatID)
+    case sampleRate(Double)
+    case channelsKey(Int)
+    case qualityKey(AVAudioQuality)
+}
+
+public extension BBRecorderOption {
+    var key: String {
+        switch self {
+        /// : 오디오 데이터의 형식을 나타내는 값
+        case .formatIDKey: return AVFormatIDKey
+        /// 샘플 속도를 헤츠르로 나타내는 부동 소수점 값
+        case .sampleRate: return AVSampleRateKey
+        /// 채널 수를 나타내는 값
+        case .channelsKey: return AVNumberOfChannelsKey
+        /// 오디오 품질을 열거형의 정수를 나타내는 상수
+        case .qualityKey: return AVEncoderAudioQualityKey
+        }
+    }
+    
+    var value: Any {
+        switch self {
+        case let .formatIDKey(formatIdKey):
+            setFormatIdKey(id: formatIdKey)
+        case let .sampleRate(hertz):
+            setSampleRate(hertz: hertz)
+        case let .channelsKey(channel):
+            setChannles(channel: channel)
+        case let .qualityKey(quality):
+            setQualityKey(quality: quality)
+        }
+    }
+}
+
+public extension BBRecorderOption {
+    /// 오디오 데이터 형식을 설정하는 메서드
+    func setFormatIdKey(id: AudioFormatID) -> Int {
+        return Int(id)
+    }
+    
+    /// 오디오 헤르츠 값을 설정하는 메서드
+    func setSampleRate(hertz: Double) -> Double {
+        return hertz
+    }
+    
+    /// 오디오 채널 수를 설정하는 메서드
+    func setChannles(channel: Int) -> Int {
+        return channel
+    }
+    
+    /// 오디오 품질을 설정하는 메서드
+    func setQualityKey(quality: AVAudioQuality) -> Int {
+        return quality.rawValue
+    }
+    
+    /// 기본 옵션 메서드
+    static func `default`() -> [BBRecorderOption] {
+        return [
+            .formatIDKey(kAudioFormatMPEG4AAC),
+            .sampleRate(12000),
+            .channelsKey(1),
+            .qualityKey(.high)
+        ]
+    }
+}
+
+
+extension Array where Element == BBRecorderOption {
+    func asFormat() -> [String: Any] {
+        var format: [String: Any] = [:]
+        for option in self {
+            format[option.key] = option.value
+        }
+        return format
+    }
+}

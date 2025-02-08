49d73dff67f0367d35f984554c5dca25799533a8
Do-hyun-Kim <dohyun8032@gmail.com>
2025-02-08 23:49:53 +0900

    fix: BBNetworkInterceptor Timeout 발생 시 Retry 할 수 있도록 로직 추가 RefreshToken Retry 코드 분기 처리

--- 14th-team5-iOS/Core/Sources/Bibbi/BBNetwork/Network/BBNetworkInterceptor.swift
+++ 14th-team5-iOS/Core/Sources/Bibbi/BBNetwork/Network/BBNetworkInterceptor.swift
@@ -15,6 +15,7 @@ import RxSwift
 public final class BBNetworkDefaultInterceptor {
     public init() { }
     private let session: BBNetworkSession = .refresh
+    private let limitRetryCount: Int = 2
 }
 
 extension BBNetworkDefaultInterceptor: RequestInterceptor {
@@ -34,32 +35,50 @@ extension BBNetworkDefaultInterceptor: RequestInterceptor {
         completion: @escaping (RetryResult) -> Void
     ) {
         
-        if let response = request.response, response.statusCode != 401 {
-            completion(.doNotRetry)
-            return
-        }
-        
-        guard let authToken: AccessToken = KeychainWrapper.standard.object(forKey: .accessToken),
-              let refreshToken = authToken.refreshToken else {
-            completion(.doNotRetry)
-            return
-        }
-        
-        var refreshedAuthToken: AccessToken? = nil
-        refreshAuthToken(refreshToken) { dataResponse in
-            
-            switch dataResponse.result {
-            case let .success(data):
-                refreshedAuthToken = data?.decode(AccessToken.self)
-                KeychainWrapper.standard.set(refreshedAuthToken, forKey: "accessToken")
-                completion(.retry)
-                
-            case let .failure(error):
-                // KeychainWrapper.standard.removeAllKeys()
-                completion(.doNotRetryWithError(error))
+        print("#️⃣ 리트라이 카운트 입니다 : \(request.retryCount) #️⃣")
+        if let error = error as? AFError {
+            print("#️⃣ 리트라이 에러 입니다 : \(request.error) #️⃣")
+            switch error {
+            case let .sessionTaskFailed(error as URLError) where error.code == .timedOut:
+                if request.retryCount < limitRetryCount {
+                    print("🟢네트워크 타임 아웃으로 인해 요청을 재시도 합니다 🟢")
+                    print("❌ 네트워크 요청한 횟수 입니다. \(request.retryCount)❌")
+                    print("🟠 네트워크 요청한 주소 입니다. \(request.request?.url) 🟠")
+                    print("😓 네트워크 타임 아웃 오류 코드입니다. \(request.error)")
+                    completion(.retry)
+                    return
+                }
+                completion(.doNotRetry)
+                return
+            default:
+                break
             }
         }
         
+        if let response = request.response, response.statusCode == 401 {
+            guard let authToken: AccessToken = KeychainWrapper.standard.object(forKey: .accessToken),
+                  let refreshToken = authToken.refreshToken else {
+                completion(.doNotRetry)
+                return
+            }
+            
+            var refreshedAuthToken: AccessToken? = nil
+            refreshAuthToken(refreshToken) { dataResponse in
+                
+                switch dataResponse.result {
+                case let .success(data):
+                    refreshedAuthToken = data?.decode(AccessToken.self)
+                    KeychainWrapper.standard.set(refreshedAuthToken, forKey: "accessToken")
+                    completion(.retry)
+                    
+                case let .failure(error):
+                    // KeychainWrapper.standard.removeAllKeys()
+                    completion(.doNotRetryWithError(error))
+                }
+            }
+        } else {
+            completion(.doNotRetry)
+        }
     }
     
 }

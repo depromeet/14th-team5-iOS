9d346ff262f0c2740610ecceeff5bc6278646c61
Do-hyun-Kim <dohyun8032@gmail.com>
2025-02-26 17:38:48 +0900

    fix: Tuist AppVersion 1.3.0 으로 수정 -  AppKey 1.3.0 버전으로 변경

--- 14th-team5-iOS/Bibbi/Project.swift
+++ 14th-team5-iOS/Bibbi/Project.swift
@@ -19,7 +19,7 @@ private let targets: [Target] = [
                 "CFBundleDisplayName": .string("Bibbi"),
                 "CFBundleVersion": .string("1"),
                 "CFBuildVersion": .string("0"),
-                "CFBundleShortVersionString": .string("1.2.6"),
+                "CFBundleShortVersionString": .string("1.3.0"),
                 "UILaunchStoryboardName": .string("LaunchScreen"),
                 "UISupportedInterfaceOrientations": .array([.string("UIInterfaceOrientationPortrait")]),
                 "UIUserInterfaceStyle": .string("Dark"),

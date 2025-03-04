7753ac7c8f2055d328e77928ade93c1779347c9d
Kyoungmi <1mmgm.dev@gmail.com>
2025-03-04 22:32:40 +0900

    feat: add notifi data

--- 14th-team5-iOS/Data/Sources/Repositories/NotificationRepository.swift
+++ 14th-team5-iOS/Data/Sources/Repositories/NotificationRepository.swift
@@ -17,11 +17,8 @@ public final class NotificationRepository: NotificationRepositoryPorotocol {
 
 extension NotificationRepository {
     public func fetchNotifications() -> Observable<[NotificationEntity]> {
-//        return notificationAPIWorker.fetchNotifications()
-//            .map { $0.map { $0.toDomain() } }
-        return .just([
-            .init(id: "123", style: "NONE", senderMemberId: "01JMY21JXST54FRRAS0A7JG22V", senderImageUrl: nil, title: "댓글 테스트얌", content: "테스트용", deepLink: "post/view/01JN34B9M9SQB6YK3ZAE2TWWN0?openComment=true&dateOfPost=2025-02-27", createdAt: "2025-02-27T16:18:45+09:00")
-        ])
+        return notificationAPIWorker.fetchNotifications()
+            .map { $0.map { $0.toDomain() } }
     }
 }
 

a6a831c9257119ff817358ab302e4a7b7d0597e1
Do-hyun-Kim <dohyun8032@gmail.com>
2026-02-18 21:19:03 +0900

    fix: MainViewReactor BallonText Type survivalStand로 수정 - Project+Templates 1.4.4 버전으로 수정

--- 14th-team5-iOS/Bibbi/Project.swift
+++ 14th-team5-iOS/Bibbi/Project.swift
@@ -82,7 +82,7 @@ private let targets: [Target] = [
                     ),
                 "CFBundleVersion": .string("1"),
                 "CFBuildVersion": .string("0"),
-                "CFBundleShortVersionString": .string("1.4.3"),
+                "CFBundleShortVersionString": .string("1.4.4"),
                 "UILaunchStoryboardName": .string("LaunchScreen"),
                 "UISupportedInterfaceOrientations": .array([.string("UIInterfaceOrientationPortrait")]),
                 "UIUserInterfaceStyle": .string("Dark"),
--- 14th-team5-iOS/Bibbi/Sources/Presentation/Home/Reactor/MainViewReactor.swift
+++ 14th-team5-iOS/Bibbi/Sources/Presentation/Home/Reactor/MainViewReactor.swift
@@ -450,7 +450,7 @@ extension MainViewReactor {
                     newState.balloonText = .pickers(currentState.pickers)
                 }
             } else {
-                newState.balloonText = .midNightStandard
+                newState.balloonText = .survivalStandard
             }
         } else {
             if !currentState.isMissionUnlocked {

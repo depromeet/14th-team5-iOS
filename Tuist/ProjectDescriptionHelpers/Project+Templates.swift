261546c144cb20bce9c4ee8fd0f49c2addee80fc
Do-hyun-Kim <dohyun8032@gmail.com>
2026-04-14 23:32:09 +0900

    fix: CFBundleShortsVersionString, Marketing Version 1.5.0 으로 수정

--- Tuist/ProjectDescriptionHelpers/Project+Templates.swift
+++ Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -59,7 +59,7 @@ extension Project {
             settings: .settings(
                 base: [
                     "OTHER_LDFLAGS": "$(inherited) -ObjC",
-                    "MARKETING_VERSION": "1.5.1",
+                    "MARKETING_VERSION": "1.5.0",
                     "CURRENT_PROJECT_VERSION": "1",
                     "VERSIONING_SYSTEM": "apple-generic"
                 ],
